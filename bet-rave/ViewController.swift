ae256c47c137809d1d0a11f85b84bb593d06d64a
tchendoh <42325308+tchendoh@users.noreply.github.com>
2024-07-14 08:57:46 -0400

    Initial Commit

---
+++ bet-rave/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  bet-rave
+//
+//  Created by Eric Chandonnet on 2024-07-14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
