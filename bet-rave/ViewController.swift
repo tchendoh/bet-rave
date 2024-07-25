d54c83b1e40bb4d2a6bfa7ec8f4008c5ddbac26c
tchendoh <42325308+tchendoh@users.noreply.github.com>
2024-07-25 16:30:36 -0400

    Enlever le code du ViewController qui ne devrait pas être encore là.

--- bet-rave/ViewController.swift
+++ bet-rave/ViewController.swift
@@ -9,28 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let sportService = SportService()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        Task {
-            await fetchFights()
-        }
     }
     
-    func fetchFights() async {
-        do {
-            let fights = try await sportService.getFights()
-            fights.forEach { fight in
-                print("\(fight.fighters.first.name) VS \(fight.fighters.second.name)")
-            }
-            
-        } catch {
-            print("Error fetching fights: \(error)")
-        }
-    }
-
 
 }
 
