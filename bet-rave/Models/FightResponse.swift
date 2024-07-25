325507efb311789a9f0c1aa81b64b89980c388a4
tchendoh <42325308+tchendoh@users.noreply.github.com>
2024-07-25 15:08:23 -0400

    Possiblement le contenu de FightResponse

--- bet-rave/Models/FightResponse.swift
+++ bet-rave/Models/FightResponse.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+
+struct FightResponse: Codable {
+    let response: [Fight]
+}
+
+struct Fight: Codable {
+    let id: Int
+    let date: String
+    let time: String
+    let timestamp: Int
+    let timezone: String
+    let slug: String
+    let category: String
+    let fighters: Fighters
+}
+
+extension Fight {
+    struct Fighters: Codable {
+        let first: Fighter
+        let second: Fighter
+    }
+    
+    struct Fighter: Codable {
+        let id: Int
+        let name: String
+        let logo: String
+        let winner: Bool
+    }
+}
